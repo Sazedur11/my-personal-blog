a1068348e84959f3bfbcf74c560ca697e323518c
Sazedur <sazedur11ab@gmail.com>
2025-07-12 23:30:25 +0600

    blog

--- src/components/blog/Blog.jsx
+++ src/components/blog/Blog.jsx
@@ -1,12 +1,29 @@
 import PropTypes from 'prop-types'; 
+import { FaRegBookmark } from 'react-icons/fa';
 
 const Blog = ({blog}) => {
     console.log(blog);
     const {author_name, author_img, banner_img, post_date, hashtag, title, reading_duration} = blog;
     return (
-        <div>
-            <h1>Title: {title}</h1>
-            <p>{hashtag}</p>
+        <div className='border-b-1 border-gray-300 mb-8'>
+            <img className='rounded-[5px]' src={banner_img} alt="Banner Image" />
+            <div className='flex items-center justify-between my-4'>
+                <div className='flex gap-4 items-center'>
+                    <img className='w-14 h-14 rounded-full' src={author_img} alt="Author Image" />
+                    <div>
+                        <h3 className='font-bold'>{author_name}</h3>
+                        <p className='text-gray-400'>{post_date}</p>
+                    </div>
+                </div>
+                <div>
+                    <span className='text-gray-400'>{reading_duration} min read</span>
+                    <button className='text-gray-400 ml-2'><FaRegBookmark></FaRegBookmark></button>
+                </div>
+            </div>
+            <h1 className='text-2xl font-bold my-2'>{title}</h1>
+            <p className='text-gray-400'><small>{hashtag}</small></p>
+            <button className='underline text-[#6047EC] mb-4'><small>Mark as read</small></button>
+            
         </div>
     );
 };
--- src/components/blogs/Blogs.jsx
+++ src/components/blogs/Blogs.jsx
@@ -13,10 +13,11 @@ const Blogs = () => {
 
     return (
         <div className="md:w-2/3">
-            <h1 className="text-3xl font-bold">This is my latest blog rr</h1>
-            <h2>Name: {blogs.length}</h2>
+            
             {
-                blogs.map(blog => <Blog blog= {blog}></Blog>)
+                blogs.map(blog => <Blog 
+                    key={blog.id}
+                    blog= {blog}></Blog>)
             }
         </div>
     );
